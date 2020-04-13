053e46b37fc244325cade3837c87bba6c8cc5f54
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-04-13 06:30:48 +0100

    ft(front-end)
    - fix landing page

--- app/api/v1/auth/authController.js
+++ app/api/v1/auth/authController.js
@@ -3,6 +3,7 @@
  */
 const authController = {
   async loginUser(req, res) {
+    const { body } = req;
     return res.status(200).send({ message: "User login" });
   },
 };
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,12 @@
 // react libraries
 import React from "react";
+import { Link } from "react-router-dom";
 
 // styles
 import "./_landing.scss";
+
+// component
+import Button from "../Common/Button";
 
 /**
  * @desc default landing page
@@ -10,180 +14,56 @@ import "./_landing.scss";
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <section className="page-section">
+      <section className="page-section top-section">
+        <h1>Bug tracking done right</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          rhoncus vulputate metus, vitae rhoncus elit tristique et. Cras sit
-          amet gravida erat. Nunc in neque eget nisi interdum convallis ac vel
-          libero. Fusce rhoncus sollicitudin lacus ut bibendum. Maecenas in sem
-          sapien. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Morbi lorem eros, euismod id turpis
-          in, molestie cursus orci. Maecenas a ex et odio suscipit blandit at in
-          lacus. Vestibulum scelerisque semper tortor et laoreet. Vivamus
-          finibus nunc tortor, nec aliquam dolor ultricies ac. Cras dui turpis,
-          pharetra eget felis id, semper scelerisque diam. Pellentesque
-          convallis quam vel ultrices porttitor. Mauris dui nisi, suscipit nec
-          elit porta, convallis pulvinar est. Curabitur pulvinar ex velit, quis
-          ultricies nibh mollis consectetur. Fusce lacus metus, sagittis sit
-          amet lorem ac, lacinia tincidunt erat. Ut ac arcu in erat dapibus
-          eleifend suscipit tristique metus. Maecenas cursus nulla arcu, eget
-          aliquam velit pharetra a. Sed vitae leo id tortor condimentum
-          elementum eget non mi. Etiam venenatis sem nec quam vulputate varius.
-          Nunc fringilla congue sapien, eu tempor arcu lobortis non. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus
-          vulputate metus, vitae rhoncus elit tristique et. Cras sit amet
-          gravida erat. Nunc in neque eget nisi interdum convallis ac vel
-          libero. Fusce rhoncus sollicitudin lacus ut bibendum. Maecenas in sem
-          sapien. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Morbi lorem eros, euismod id turpis
-          in, molestie cursus orci. Maecenas a ex et odio suscipit blandit at in
-          lacus. Vestibulum scelerisque semper tortor et laoreet. Vivamus
-          finibus nunc tortor, nec aliquam dolor ultricies ac. Cras dui turpis,
-          pharetra eget felis id, semper scelerisque diam. Pellentesque
-          convallis quam vel ultrices porttitor. Mauris dui nisi, suscipit nec
-          elit porta, convallis pulvinar est. Curabitur pulvinar ex velit, quis
-          ultricies nibh mollis consectetur. Fusce lacus metus, sagittis sit
-          amet lorem ac, lacinia tincidunt erat. Ut ac arcu in erat dapibus
-          eleifend suscipit tristique metus. Maecenas cursus nulla arcu, eget
-          aliquam velit pharetra a. Sed vitae leo id tortor condimentum
-          elementum eget non mi. Etiam venenatis sem nec quam vulputate varius.
-          Nunc fringilla congue sapien, eu tempor arcu lobortis non. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Integer a euismod risus.
-          Nunc aliquet est elit, quis vestibulum quam eleifend euismod. Nullam
-          fermentum pharetra urna. Morbi ac mattis erat, eget ullamcorper arcu.
-          Pellentesque porta congue erat, sit amet vestibulum leo tempor et.
-          Duis vitae justo accumsan, volutpat leo in, facilisis ex. Pellentesque
-          semper dolor ut elementum commodo. Quisque ultrices metus in porta
-          venenatis. Mauris accumsan malesuada scelerisque. Sed volutpat sit
-          amet ante ut aliquet. Cras venenatis molestie malesuada. Nulla pretium
-          blandit odio sit amet feugiat. Aliquam in sapien eu felis elementum
-          accumsan. Suspendisse suscipit imperdiet libero sed euismod.Curabitur
-          semper rhoncus sem sit amet molestie. Sed at enim sit amet tellus
-          dignissim consectetur. Fusce consequat et nibh facilisis facilisis.
-          Aliquam in interdum magna. Praesent ac tortor sem. Sed aliquet rutrum
-          laoreet. Suspendisse efficitur lacus vitae suscipit convallis. Sed
-          sollicitudin urna at lectus efficitur, at facilisis magna viverra.
-          Pellentesque eget luctus ligula. Aenean convallis massa non
-          scelerisque ullamcorper. Fusce at elementum nulla. Fusce in bibendum
-          massa, et tristique libero. Nulla luctus massa id luctus iaculis.
-          Curabitur ornare diam ante, eu tempor nibh dignissim at.
+          Capture, track and resolve bugs & issues throughout your entire
+          development
         </p>
+        <Link to="/register">
+          <Button btnText="learn more" />
+        </Link>
       </section>
-      <section className="page-section">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          rhoncus vulputate metus, vitae rhoncus elit tristique et. Cras sit
-          amet gravida erat. Nunc in neque eget nisi interdum convallis ac vel
-          libero. Fusce rhoncus sollicitudin lacus ut bibendum. Maecenas in sem
-          sapien. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Morbi lorem eros, euismod id turpis
-          in, molestie cursus orci. Maecenas a ex et odio suscipit blandit at in
-          lacus. Vestibulum scelerisque semper tortor et laoreet. Vivamus
-          finibus nunc tortor, nec aliquam dolor ultricies ac. Cras dui turpis,
-          pharetra eget felis id, semper scelerisque diam. Pellentesque
-          convallis quam vel ultrices porttitor. Mauris dui nisi, suscipit nec
-          elit porta, convallis pulvinar est. Curabitur pulvinar ex velit, quis
-          ultricies nibh mollis consectetur. Fusce lacus metus, sagittis sit
-          amet lorem ac, lacinia tincidunt erat. Ut ac arcu in erat dapibus
-          eleifend suscipit tristique metus. Maecenas cursus nulla arcu, eget
-          aliquam velit pharetra a. Sed vitae leo id tortor condimentum
-          elementum eget non mi. Etiam venenatis sem nec quam vulputate varius.
-          Nunc fringilla congue sapien, eu tempor arcu lobortis non. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus
-          vulputate metus, vitae rhoncus elit tristique et. Cras sit amet
-          gravida erat. Nunc in neque eget nisi interdum convallis ac vel
-          libero. Fusce rhoncus sollicitudin lacus ut bibendum. Maecenas in sem
-          sapien. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Morbi lorem eros, euismod id turpis
-          in, molestie cursus orci. Maecenas a ex et odio suscipit blandit at in
-          lacus. Vestibulum scelerisque semper tortor et laoreet. Vivamus
-          finibus nunc tortor, nec aliquam dolor ultricies ac. Cras dui turpis,
-          pharetra eget felis id, semper scelerisque diam. Pellentesque
-          convallis quam vel ultrices porttitor. Mauris dui nisi, suscipit nec
-          elit porta, convallis pulvinar est. Curabitur pulvinar ex velit, quis
-          ultricies nibh mollis consectetur. Fusce lacus metus, sagittis sit
-          amet lorem ac, lacinia tincidunt erat. Ut ac arcu in erat dapibus
-          eleifend suscipit tristique metus. Maecenas cursus nulla arcu, eget
-          aliquam velit pharetra a. Sed vitae leo id tortor condimentum
-          elementum eget non mi. Etiam venenatis sem nec quam vulputate varius.
-          Nunc fringilla congue sapien, eu tempor arcu lobortis non. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus
-          vulputate metus, vitae rhoncus elit tristique et. Cras sit amet
-          gravida erat. Nunc in neque eget nisi interdum convallis ac vel
-          libero. Fusce rhoncus sollicitudin lacus ut bibendum. Maecenas in sem
-          sapien. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Morbi lorem eros, euismod id turpis
-          in, molestie cursus orci. Maecenas a ex et odio suscipit blandit at in
-          lacus. Vestibulum scelerisque semper tortor et laoreet. Vivamus
-          finibus nunc tortor, nec aliquam dolor ultricies ac. Cras dui turpis,
-          pharetra eget felis id, semper scelerisque diam. Pellentesque
-          convallis quam vel ultrices porttitor. Mauris dui nisi, suscipit nec
-          elit porta, convallis pulvinar est. Curabitur pulvinar ex velit, quis
-          ultricies nibh mollis consectetur. Fusce lacus metus, sagittis sit
-          amet lorem ac, lacinia tincidunt erat. Ut ac arcu in erat dapibus
-          eleifend suscipit tristique metus. Maecenas cursus nulla arcu, eget
-          aliquam velit pharetra a. Sed vitae leo id tortor condimentum
-          elementum eget non mi. Etiam venenatis sem nec quam vulputate varius.
-          Nunc fringilla congue sapien, eu tempor arcu lobortis non.
-        </p>
+      <section className="page-section middle-section">
+        <section className="section-text">
+          <h2>What is a bug and issue tracking tool?</h2>
+          <p>
+            Bug and issue tracking software helps software teams find, record
+            and track bugs in their software.A great bug & issue tracker gives
+            the team a single view of all items in the backlog.
+          </p>
+        </section>
+        <section className="section-text">
+          <h2>Logan Bug and Issue Tracker</h2>
+          <p>
+            Logan was built to track and manage bugs in personal projects by
+            Oluwafemi Akinwa in 2020. Still in beta but hopefully would go live
+            with cool features before the end of the year
+          </p>
+        </section>
       </section>
-      <section className="page-section">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          rhoncus vulputate metus, vitae rhoncus elit tristique et. Cras sit
-          amet gravida erat. Nunc in neque eget nisi interdum convallis ac vel
-          libero. Fusce rhoncus sollicitudin lacus ut bibendum. Maecenas in sem
-          sapien. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Morbi lorem eros, euismod id turpis
-          in, molestie cursus orci. Maecenas a ex et odio suscipit blandit at in
-          lacus. Vestibulum scelerisque semper tortor et laoreet. Vivamus
-          finibus nunc tortor, nec aliquam dolor ultricies ac. Cras dui turpis,
-          pharetra eget felis id, semper scelerisque diam. Pellentesque
-          convallis quam vel ultrices porttitor. Mauris dui nisi, suscipit nec
-          elit porta, convallis pulvinar est. Curabitur pulvinar ex velit, quis
-          ultricies nibh mollis consectetur. Fusce lacus metus, sagittis sit
-          amet lorem ac, lacinia tincidunt erat. Ut ac arcu in erat dapibus
-          eleifend suscipit tristique metus. Maecenas cursus nulla arcu, eget
-          aliquam velit pharetra a. Sed vitae leo id tortor condimentum
-          elementum eget non mi. Etiam venenatis sem nec quam vulputate varius.
-          Nunc fringilla congue sapien, eu tempor arcu lobortis non. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus
-          vulputate metus, vitae rhoncus elit tristique et. Cras sit amet
-          gravida erat. Nunc in neque eget nisi interdum convallis ac vel
-          libero. Fusce rhoncus sollicitudin lacus ut bibendum. Maecenas in sem
-          sapien. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Morbi lorem eros, euismod id turpis
-          in, molestie cursus orci. Maecenas a ex et odio suscipit blandit at in
-          lacus. Vestibulum scelerisque semper tortor et laoreet. Vivamus
-          finibus nunc tortor, nec aliquam dolor ultricies ac. Cras dui turpis,
-          pharetra eget felis id, semper scelerisque diam. Pellentesque
-          convallis quam vel ultrices porttitor. Mauris dui nisi, suscipit nec
-          elit porta, convallis pulvinar est. Curabitur pulvinar ex velit, quis
-          ultricies nibh mollis consectetur. Fusce lacus metus, sagittis sit
-          amet lorem ac, lacinia tincidunt erat. Ut ac arcu in erat dapibus
-          eleifend suscipit tristique metus. Maecenas cursus nulla arcu, eget
-          aliquam velit pharetra a. Sed vitae leo id tortor condimentum
-          elementum eget non mi. Etiam venenatis sem nec quam vulputate varius.
-          Nunc fringilla congue sapien, eu tempor arcu lobortis non. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus
-          vulputate metus, vitae rhoncus elit tristique et. Cras sit amet
-          gravida erat. Nunc in neque eget nisi interdum convallis ac vel
-          libero. Fusce rhoncus sollicitudin lacus ut bibendum. Maecenas in sem
-          sapien. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Morbi lorem eros, euismod id turpis
-          in, molestie cursus orci. Maecenas a ex et odio suscipit blandit at in
-          lacus. Vestibulum scelerisque semper tortor et laoreet. Vivamus
-          finibus nunc tortor, nec aliquam dolor ultricies ac. Cras dui turpis,
-          pharetra eget felis id, semper scelerisque diam. Pellentesque
-          convallis quam vel ultrices porttitor. Mauris dui nisi, suscipit nec
-          elit porta, convallis pulvinar est. Curabitur pulvinar ex velit, quis
-          ultricies nibh mollis consectetur. Fusce lacus metus, sagittis sit
-          amet lorem ac, lacinia tincidunt erat. Ut ac arcu in erat dapibus
-          eleifend suscipit tristique metus. Maecenas cursus nulla arcu, eget
-          aliquam velit pharetra a. Sed vitae leo id tortor condimentum
-          elementum eget non mi. Etiam venenatis sem nec quam vulputate varius.
-          Nunc fringilla congue sapien, eu tempor arcu lobortis non.
-        </p>
+      <section className="page-section bottom-section">
+        <section className="btm-section-item">
+          <h4>Developer tool intgrations</h4>
+          <p>
+            Connect to github to get an end-to-end traceability from backlog to
+            deployment
+          </p>
+        </section>
+        <section className="btm-section-item">
+          <h4>Easy to use APIs</h4>
+          <p>
+            Integrate Logan with all the tools your team is already using by
+            leveraging its robust set of APIs.
+          </p>
+        </section>
+        <section className="btm-section-item">
+          <h4>Third-party addons</h4>
+          <p>
+            Install plug-and-play third-party addons to extend Logan to fit any
+            use case.
+          </p>
+        </section>
       </section>
     </div>
   );
