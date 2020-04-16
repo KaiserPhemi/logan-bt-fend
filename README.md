b69faba6db90005d31f6dd1d2ef3ff9c9b336f18
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-04-16 07:57:45 +0100

    ch(front-end)
    - add test
    - add coverage script
    - add readme badge

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![Coverage Status](https://coveralls.io/repos/github/KaiserPhemi/logan/badge.svg?branch=master)](https://coveralls.io/github/KaiserPhemi/logan?branch=master)
+
 # logan
+
 A bug and issue management app
---
+++ src/components/Footer/__tests__/Footer.test.jsx
@@ -0,0 +1,12 @@
+import "@testing-library/jest-dom/extend-expect";
+// import { render, fireEvent, screen } from "@testing-library/react";
+
+// react libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// component
+import Footer from "../Footer";
+
+// test cases
+test("Render the footer", () => {});
