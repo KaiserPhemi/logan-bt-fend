8b1b518f29db65b67b0316479e6dcac9d4e5b948
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-05-11 17:42:26 +0100

    ft(backend)
    - add project model

--- app/api/v1/project/projectModel.js
+++ app/api/v1/project/projectModel.js
@@ -3,7 +3,21 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 // schema
-const projectModel = new Schema({});
+const projectModel = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+
+  team: {
+    type: String,
+    required: true,
+  },
+});
 
 // model
 const Project = model("Project", projectModel);
